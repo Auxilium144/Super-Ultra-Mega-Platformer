14975bfdc2608f3ce91b3beb3de1a91fe2ecddaa
grahm-manley <grahm.manley@gmail.com>
2017-03-12 21:11:46 -0600

    Start Screen & stage placeholder
    Imported some unity standard assets and made an offensively ugly but
    working start screen that has a button to send you to stage one.

---
+++ Super-Ultra-Mega Platformer/Assets/menuScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menuScript : MonoBehaviour {
+
+	public Button scene1Text;
+
+	// Use this for initialization
+	void Start () {
+		scene1Text = scene1Text.GetComponent<Button> ();
+	}
+	
+	public void scene1Press()
+	{
+		Application.LoadLevel (1);
+	}
+}
