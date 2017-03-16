d101ddc4fe45143c20ad77175a6ee344381afff7
SilverShadowx <ericsilver123@gmail.com>
2017-03-16 18:52:53 -0500

    Abilities keys ready
    - all 4 keys are functional and switchable
    - Some Placeholder Abilities in
    - PreMerge to Master
    - Changed scripts significantly.
    Eric Tran

--- Super-Ultra-Mega Platformer/Assets/Scripts/DoubleJump.cs
+++ Super-Ultra-Mega Platformer/Assets/Scripts/DoubleJump.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaterMoveSlow : MonoBehaviour {
+public class DoubleJump : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
--- Super-Ultra-Mega Platformer/Assets/Scripts/PlatformerCharacter2D.cs
+++ Super-Ultra-Mega Platformer/Assets/Scripts/PlatformerCharacter2D.cs
@@ -8,18 +8,23 @@ namespace UnityStandardAssets._2D
         [SerializeField] private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
         [SerializeField] private float m_JumpForce = 400f;                  // Amount of force added when the player jumps.
         [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;  // Amount of maxSpeed applied to crouching movement. 1 = 100%
+        [Range(0, 1)] [SerializeField] private float m_WaterSpeed = .36f;  // Amount of maxSpeed applied to crouching movement. 1 = 100%
         [SerializeField] private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
         [SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
+        [SerializeField] private LayerMask m_WhatIsWater;                  // A mask determining what is water to the character
+        [SerializeField] protected bool AbilityEnable;                  // are abilities allowed?
 
         private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
         const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
         private bool m_Grounded;            // Whether or not the player is grounded.
+        private bool m_Water;            // Whether or not the player is Watered.
         private Transform m_CeilingCheck;   // A position marking where to check for ceilings
         const float k_CeilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
         private Animator m_Anim;            // Reference to the player's animator component.
         private Rigidbody2D m_Rigidbody2D;
         private bool m_FacingRight = true;  // For determining which way the player is currently facing.
         private int m_jumps = 0; //holds jump charges
+        private int NumJumps = 0;
         //private bool m_doubleJumpEnable; //enables double jumps
         private void Awake()
         {
@@ -30,18 +35,43 @@ namespace UnityStandardAssets._2D
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
         }
 
-
+        public bool getAbilityEnable()
+        {
+            return AbilityEnable;
+        }
+        public void setNumJumps(int pie)
+        {
+            if(pie == 0)
+            {
+                NumJumps = 1;
+            }
+            else
+            {
+                NumJumps = 0;
+            }
+        }
         private void FixedUpdate()
         {
             m_Grounded = false;
+            m_Water = false;
 
-            // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+            // The player is grounded or water if a circlecast to the groundcheck position hits anything designated as ground or water
             // This can be done using layers instead but Sample Assets will not overwrite your project settings.
             Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
             for (int i = 0; i < colliders.Length; i++)
             {
                 if (colliders[i].gameObject != gameObject)
+                {
                     m_Grounded = true;
+                }
+            }
+            Collider2D[] waters = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsWater);
+            for (int i = 0; i < waters.Length; i++)
+            {
+                if (waters[i].gameObject != gameObject)
+                {
+                    m_Water = true;
+                }
             }
             m_Anim.SetBool("Ground", m_Grounded);
 
@@ -50,8 +80,9 @@ namespace UnityStandardAssets._2D
         }
 
 
-        public void Move(float move, bool crouch, bool jump, bool doubleJump)
+        public void Move(float move, bool crouch, bool jump, bool[] AbilityList)
         {
+            
             // If crouching, check to see if the character can stand up
             if (!crouch && m_Anim.GetBool("Crouch"))
             {
@@ -65,18 +96,18 @@ namespace UnityStandardAssets._2D
             // Set whether or not the character is crouching in the animator
             m_Anim.SetBool("Crouch", crouch);
 
-            //only control the player if grounded or airControl is turned on
-            if (m_Grounded || m_AirControl)
+            //only control the player if grounded, airControl, m_Water is turned on
+            if (m_Grounded || m_AirControl || m_Water)
             {
                 // Reduce the speed if crouching by the crouchSpeed multiplier
                 move = (crouch ? move*m_CrouchSpeed : move);
-
+                // Reduce the speed if in water by the water multiplier
+                if (m_Water)
+                {
+                    move = (move * m_WaterSpeed);
+                }
                 // The Speed animator parameter is set to the absolute value of the horizontal input.
                 m_Anim.SetFloat("Speed", Mathf.Abs(move));
-
-                // Move the character
-                m_Rigidbody2D.velocity = new Vector2(move*m_MaxSpeed, m_Rigidbody2D.velocity.y);
-
                 // If the input is moving the player right and the player is facing left...
                 if (move > 0 && !m_FacingRight)
                 {
@@ -91,19 +122,44 @@ namespace UnityStandardAssets._2D
                 }
             }
             // If the player should jump...
-            if (m_Grounded && jump && m_Anim.GetBool("Ground"))
+            if (m_Water && jump && m_Anim.GetBool("Ground"))
+            {
+                // reduced jump velocity due to water
+                m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce/4));
+                m_jumps = 1;
+                Debug.Log("jump from water");
+            }
+            else if (m_Grounded && jump && m_Anim.GetBool("Ground"))
             {
                 // Add a vertical force to the player.
                 m_Grounded = false;
                 m_Anim.SetBool("Ground", false);
                 m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
-                m_jumps = 1;
+                Debug.Log("jump from ground");
+                m_jumps = NumJumps;
             }
-            else if (!m_Grounded && (m_jumps > 0) && doubleJump) //&& !m_Anim.GetBool("Ground")
+            else if (!m_Grounded && (m_jumps > 0) && AbilityList[0]) //&& !m_Anim.GetBool("Ground")
             {
                 m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce/2));
                 m_jumps = m_jumps - 1;
+                Debug.Log("jump from air");
             }
+            if(m_Grounded && AbilityList[1]) // dash forward/right
+            {
+                m_Rigidbody2D.AddForce(new Vector2(1000f, 0f));
+            }
+            if (m_Grounded && AbilityList[2]) // dash backwards/left
+            {
+                m_Rigidbody2D.AddForce(new Vector2(-1000f, 0f));
+            }
+            if (m_Grounded && AbilityList[3]) // dash forward/right
+            {
+                m_Grounded = false;
+                m_Rigidbody2D.AddForce(new Vector2(1000f, 400f));
+            }
+            // Move the character
+            m_Rigidbody2D.velocity = new Vector2(move * m_MaxSpeed, m_Rigidbody2D.velocity.y);
+            Debug.Log("The current move value is equal to " + move * m_MaxSpeed);
         }
 
 
@@ -119,3 +175,17 @@ namespace UnityStandardAssets._2D
         }
     }
 }
+
+/*
+ * 0 Double Jump
+ * 1 Dash Forward (PlaceHolder)
+ * 2 Dash Backward (PlaceHolder)
+ * 3 Lunge right(PlaceHolder)
+ * 4
+ * 5
+ * 6
+ * 7
+ * 8
+ * 9
+ * 10
+ */
