67fb03aedd2794b616974d296f2308616fd05a32
grahm-manley <grahm.manley@gmail.com>
2017-03-16 21:31:52 -0600

    Abilities selection stage and menu changes
    Created abilities selection stage, changed fonts, changed build stage
    numbers

---
+++ Super-Ultra-Mega Platformer/Assets/Scripts/AbilityButtonScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityButtonScript : MonoBehaviour {
+
+	public Button ability1;
+	public Button ability2;
+	public Button ability3;
+	public Button ability4;
+	private int abilitiesCount;
+	
+	void Start () 
+	{
+		ability1 = ability1.GetComponent<Button> ();
+		ability2 = ability2.GetComponent<Button> ();
+		ability3 = ability3.GetComponent<Button> ();
+		ability4 = ability4.GetComponent<Button> ();
+		abilitiesCount = 0;
+	}
+	
+	public void ability1Press()
+	{
+		if(abilitiesCount <= 2) 
+		{
+			
+		}
+		ability1.enabled = false;
+	}
+	
+	public void ability2Press()
+	{
+		ability2.enabled = false;
+	}
+	
+	public void ability3Press()
+	{
+		ability3.enabled = false;
+	}
+	
+	public void ability4Press()
+	{
+		ability4.enabled = false;
+	}
+	
+}
