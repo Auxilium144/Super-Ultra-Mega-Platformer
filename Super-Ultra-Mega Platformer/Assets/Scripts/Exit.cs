26662eaa1f2313190ce3e91279cb72be193040ee
grahm-manley <grahm.manley@gmail.com>
2017-04-23 22:14:05 -0600

    Deleted ui elements to re add from previous branches

--- Super-Ultra-Mega Platformer/Assets/Scripts/Exit.cs
+++ Super-Ultra-Mega Platformer/Assets/Scripts/Exit.cs
@@ -1,17 +1,34 @@
-﻿using System.Collections;
+﻿﻿using System;
+using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour {
 	public Canvas exitCanvas;
 	private bool exitCanvasOn = false;
 	// Use this for initialization
 	void Start () {
-		
+		exitCanvas.GetComponent<Canvas> ().enabled = exitCanvasOn;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKeyDown(KeyCode.Escape)) {
+			Debug.Log("Escape");
+			exitCanvasOn = !exitCanvasOn;
+			exitCanvas.GetComponent<Canvas> ().enabled = exitCanvasOn;
+		}
+	}
+	public void backToGamePress() {
+		exitCanvasOn = !exitCanvasOn;
+		exitCanvas.GetComponent<Canvas> ().enabled = exitCanvasOn;
+	}
+	
+	public void mainMenuPress() {
+		SceneManager.LoadScene("menu");
 	}
 }
--- Super-Ultra-Mega Platformer/Assets/Scripts/Hud.cs
+++ Super-Ultra-Mega Platformer/Assets/Scripts/Hud.cs
@@ -13,25 +13,38 @@ public class Hud : MonoBehaviour {
 	public Text abilityA;
 	public Text abilityS;
 	public Text abilityD;
-	private string[] abilityNames = {"Double Jump", "Dash Forward", "Blink", "Lunge", "Bounce", "Wall Jump", "Wall Climb"};
+	private string[] abilityNames = {"Double Jump", "Dash Forward", "Blink", "Float", "Bounce", "Wall Jump", "Wall Climb", "Heavy"};
 	private string[] keyAbilities ={"No Ability", "No Ability", "No Ability", "No Ability"};
 	private int[] abilityList = {-1, -1, -1, -1};
+	Color oldColor;
 	
 	void Start () {
 		getAbilityList();
-		int count = 0;
+		//int count = 0;
+		int i = 0;
+		while(abilityList[i] != -1 && i <= 3) {
+			keyAbilities[i] = abilityNames[abilityList[i]];
+			i++;
+		}
+		/*
 		foreach(int i in abilityList) {
 			keyAbilities[count] = abilityNames[abilityList[count]];
 			count += 1;
 		}
-		// Below are not the correct number assignments
-		abilityW.text = keyAbilities[3];
-		//abilityA = GetComponent<Text>();
-		abilityA.text = keyAbilities[0];
-		//abilityS = GetComponent<Text>();
-		abilityS.text = keyAbilities[1];
-		//abilityD = GetComponent<Text>();
-		abilityD.text = keyAbilities[2];
+		*/
+		if(abilityList[0] != -1) {
+			abilityA.text = keyAbilities[0];
+		}
+		if(abilityList[1] != -1) {
+			abilityS.text = keyAbilities[1];
+		}
+		if(abilityList[2] != -1) {
+			abilityD.text = keyAbilities[2];
+		}
+		if(abilityList[3] != -1) {
+			abilityW.text = keyAbilities[3];
+		}
+		oldColor = abilityA.color;
 	}
 	
 	private void Update() 
@@ -43,6 +56,34 @@ public class Hud : MonoBehaviour {
 			abilityCanvas.GetComponent<Canvas> ().enabled = abilityCanvasOn;
 			
 		}
+		if(abilityCanvasOn) {
+			
+			if(CrossPlatformInputManager.GetButtonDown("Ability0")) {
+				abilityA.color = Color.white;
+				abilityS.color = oldColor;
+				abilityD.color = oldColor;
+				abilityW.color = oldColor;
+				
+			}
+			if(CrossPlatformInputManager.GetButtonDown("Ability1")) {
+				abilityS.color = Color.white;
+				abilityA.color = oldColor;
+				abilityD.color = oldColor;
+				abilityW.color = oldColor;
+			}
+			if(CrossPlatformInputManager.GetButtonDown("Ability2")) {
+				abilityD.color = Color.white;
+				abilityA.color = oldColor;
+				abilityS.color = oldColor;
+				abilityW.color = oldColor;
+			}
+			if(CrossPlatformInputManager.GetButtonDown("Ability3")) {
+				abilityW.color = Color.white;
+				abilityA.color = oldColor;
+				abilityD.color = oldColor;
+				abilityS.color = oldColor;
+			}
+		}
 	}
 	
 	
@@ -50,10 +91,11 @@ public class Hud : MonoBehaviour {
 	{
 		string path = Directory.GetCurrentDirectory();
 		string[] lines = System.IO.File.ReadAllLines(path + "abilities.txt");
-		for (int i = 0; i <= 3; i++) 
+		
+		for (int i = 0; i <= 3 && (lines[i] != ""); i++) 
 		{
-			abilityList[i] = Int32.Parse(lines[i]);
-			Debug.Log("Stuff" + lines[i]);
+				abilityList[i] = (int) Int32.Parse(lines[i]);
+				Debug.Log("Stuff" + lines[i]);
 		}
 		
 	}
